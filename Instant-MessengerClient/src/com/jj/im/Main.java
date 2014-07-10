cf5a8e458590417070a1619b662063384cb4ac25
Joseph314 <joseph5192000@gmail.com>
2014-07-10 14:21:49 -0600

    Bug fix
    Forgot to instantiate OutputThread

--- Instant-MessengerClient/src/com/jj/im/Main.java
+++ Instant-MessengerClient/src/com/jj/im/Main.java
@@ -3,6 +3,7 @@ package com.jj.im;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -16,11 +17,13 @@ public class Main {
 		}
 		String hostName = args[0];
 		int portNumber = Integer.parseInt(args[1]);
-		
 		try (
 			Socket imSocket = new Socket(hostName,portNumber);
+			PrintWriter out = new PrintWriter(imSocket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(imSocket.getInputStream()));
 		) {
+			OutputThread output = new OutputThread(out);
+			output.start();
 			String fromServer;
 			in.ready();
 			while ((fromServer = in.readLine()) != null) {
